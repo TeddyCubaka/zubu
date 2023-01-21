fecd4bbbf24943422f6fbbdbc2797d6727457712
teddy cubaka <teddycubaka96@gmail.com>
2023-01-21 18:30:24 +0100

    display of forms according to the size of the count variable in the store

--- components/components/addPropretyForm.tsx
+++ components/components/addPropretyForm.tsx
@@ -1,13 +1,23 @@
 import React from "react";
 import { GetAdress, GetLosor, GetPrice, GetPropretyType } from "./PrpretysData";
+import { publicationStore } from "../../store/publicationStore";
+import { shallow } from "zustand/shallow";
 
 export default function AddPropretiyForm() {
+	const [count] = publicationStore((state) => [state.count], shallow);
 	return (
 		<div>
-			<GetAdress />
-			<GetPropretyType />
-			<GetLosor />
-			<GetPrice />
+			{count === 0 ? (
+				<GetAdress />
+			) : count === 1 ? (
+				<GetPropretyType />
+			) : count === 2 ? (
+				<GetLosor />
+			) : count === 3 ? (
+				<GetPrice />
+			) : (
+				"fin"
+			)}
 		</div>
 	);
 }
