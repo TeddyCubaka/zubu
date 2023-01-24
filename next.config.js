93fb03d7235e391ab1a76d6a24ad65228e3e5b14
teddy cubaka <teddycubaka96@gmail.com>
2023-01-24 17:40:56 +0100

    Config the image function in the next image file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+};
+module.exports = nextConfig;
 
-module.exports = nextConfig
+module.exports = {
+	images: {
+		domains: [
+			"unsplash.com",
+			"miro.medium.com",
+			"www.shutterstock.com",
+			"play-lh.googleusercontent.com",
+		],
+	},
+};
