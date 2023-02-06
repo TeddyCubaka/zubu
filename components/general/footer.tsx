6f914ea16d0044a791e0f776bf137a3df77f6b16
teddy cubaka <teddycubaka96@gmail.com>
2023-02-06 13:31:14 +0100

    create the partial footer component

---
+++ components/general/footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+	return (
+		<div className="footer flex_y_center-xy ">
+			<span className="color_w">Footer</span>
+		</div>
+	);
+}
