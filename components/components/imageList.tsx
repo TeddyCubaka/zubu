9b00a9af36ddf42710fcf99660b1fabfda5aaf96
teddy cubaka <teddycubaka96@gmail.com>
2023-03-09 14:38:44 +0100

    update the images default size

--- components/components/imageList.tsx
+++ components/components/imageList.tsx
@@ -51,8 +51,8 @@ function PropretyImage(props: ImageProps) {
 				ref={imageRef}
 				className="h_auto w_max"
 				src={props.source}
-				width={100}
-				height={100}
+				width={300}
+				height={300}
 				alt={props.description}
 			/>
 		</div>
