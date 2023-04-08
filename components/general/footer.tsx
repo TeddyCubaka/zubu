61956649454efaeadb111eb2c65b100001518d3a
teddy cubaka <teddycubaka96@gmail.com>
2023-04-08 17:44:29 +0100

    create: provisional footer

--- components/general/footer.tsx
+++ components/general/footer.tsx
@@ -2,8 +2,9 @@ import React from "react";
 
 export default function Footer() {
 	return (
-		<div className="footer flex_y_center-xy ">
-			<span className="color_w">Footer</span>
+		<div className="footer flex_y_center-xy pd_y-20 color_w">
+			<h1 className="h_max flex_y_center-xy">Zubu</h1>
+			<span>2023@copyright</span>
 		</div>
 	);
 }
