4f329fb2ec62f2695d4e57e224a0defb6eeba5c9
teddy cubaka <teddycubaka96@gmail.com>
2023-04-09 16:42:23 +0100

    create : user information card

--- components/components/userComponents.tsx
+++ components/components/userComponents.tsx
@@ -3,9 +3,88 @@ import { useEffect, useState } from "react";
 import { userStore } from "../../store/user";
 import { Proprety } from "../interface/proprety";
 import { AskToServerDataType } from "../interface/requests";
-import { User } from "../interface/user";
 import { askToServerData } from "../usefulFuction/requests";
 import PropretyCard from "./propretyViewCard";
+import {
+	MdMailOutline,
+	MdLocalPhone,
+	MdHome,
+	MdMale,
+	MdFemale,
+} from "react-icons/md";
+import { IoAlert } from "react-icons/io5";
+import { FaUserTie } from "react-icons/fa";
+import { shallow } from "zustand/shallow";
+import Image from "next/image";
+import { IconType } from "react-icons";
+
+interface TextWithIconType {
+	Icon: IconType;
+	content: string;
+}
+
+const TextWithIcon = ({ Icon, content }: TextWithIconType) => {
+	return (
+		<span className="flex_x-center">
+			<Icon className="m_right-10" /> {content}{" "}
+		</span>
+	);
+};
+
+export function UserInformation() {
+	const [currentUser, _setCurrentUser] = userStore(
+		(store) => [store.currentUser, store._setCurrentUser],
+		shallow
+	);
+	useEffect(() => {
+		if (window !== undefined) {
+			const currentUserHasString: string | null = localStorage.getItem("user");
+			_setCurrentUser(
+				JSON.parse(currentUserHasString !== null ? currentUserHasString : "")
+			);
+		}
+	}, []);
+	return (
+		<div
+			style={{ height: "200px", width: "100%" }}
+			className="border-b flex m_y-20">
+			<div
+				style={{ height: "180px", width: "180px" }}
+				className="flex_center-xy  m-10 border-b_thin">
+				{currentUser.profile_picture_url &&
+				currentUser.profile_picture_url.length > 0 ? (
+					<Image
+						alt={"Photo de profil zubu de " + currentUser.username}
+						src={currentUser.profile_picture_url}
+						height={300}
+						width={300}
+					/>
+				) : (
+					<FaUserTie size={"150px"} />
+				)}
+			</div>
+			<div className="m_y-10 w_max space_between-y">
+				<div className="flex_x-center">
+					<h2 className="w_max">{currentUser.username}</h2>
+					<div
+						style={{ borderRadius: "80px" }}
+						className="pd-5 color_blue border-blue">
+						{currentUser.gender === "Homme" ? <MdMale size={25} /> : ""}
+						{currentUser.gender === "Femme" ? <MdFemale size={25} /> : ""}
+						{currentUser.gender === "Autre" ? <IoAlert size={25} /> : ""}
+					</div>
+				</div>
+				<TextWithIcon Icon={MdMailOutline} content={currentUser.mail} />
+				<TextWithIcon Icon={MdLocalPhone} content={currentUser.phone_number} />
+				<TextWithIcon
+					Icon={MdHome}
+					content={currentUser.proprety.length + " propriété(s)"}
+				/>
+				<TextWithIcon Icon={MdMailOutline} content={currentUser.created_at} />
+			</div>
+		</div>
+	);
+}
 
 export function GetUserPropreties() {
 	const user = userStore();
--- pages/user/index.tsx
+++ pages/user/index.tsx
@@ -1,3 +1,35 @@
+import Head from "next/head";
+import {
+	GetUserPropreties,
+	UserInformation,
+} from "../../components/components/userComponents";
+import Footer from "../../components/general/footer";
+import Header from "../../components/general/header";
+
 export default function User() {
-	return <div></div>;
+	return (
+		<>
+			<Head>
+				<title>Zubu</title>
+				<meta
+					name="description"
+					content="Télécharger votre propriété sur la forme Zubu et elle sera prête pour une annonce"
+				/>
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<main>
+				<Header />
+				<div className="m_x-20">
+					<UserInformation />
+					<h1>Vos propriétés</h1>
+					<div className="m_y-20">
+						Appuiez sur une propriété pour gérer ses informations
+					</div>
+					<GetUserPropreties />
+				</div>
+				<Footer />
+			</main>
+		</>
+	);
 }
