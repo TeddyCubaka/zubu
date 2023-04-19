7afcee0d3ab78076552dee9d1bdb48d3ef687c8a
teddy cubaka <teddycubaka96@gmail.com>
2023-04-19 18:26:10 +0100

    create : get propreties by a filter

--- components/general/main.tsx
+++ components/general/main.tsx
@@ -2,9 +2,27 @@ import { useRouter } from "next/router";
 import React from "react";
 import background from "../images/backgournd.png";
 
-export default function Main() {
+function FilterPropretiesSearch() {
 	const router = useRouter();
+	return (
+		<div className="filter_card">
+			<div className="br border-w m_y-20 pd-10">
+				<span className="m_x-20">Type</span>
+				<span className="m_x-20">Prix</span>
+				<span className="m_x-20">Localisation</span>
+				<span className="m_x-20">Ville</span>
+				<span className="m_x-20">Pièces</span>
+			</div>
+			<button
+				className="btn_p btn color_w br txt_normal m_x-10"
+				onClick={() => router.push("/proprety")}>
+				Trouver une maison
+			</button>
+		</div>
+	);
+}
 
+export default function Main() {
 	return (
 		<div
 			className="main"
@@ -18,20 +36,7 @@ export default function Main() {
 				<div className="m_y-20">
 					Recherchez un bien, trouvez-le et fait de ça votre chez vous
 				</div>
-				<div className="filter_card">
-					{/* <div className="br border-w m_y-20 pd-10">
-						<span className="m_x-20">Type</span>
-						<span className="m_x-20">Prix</span>
-						<span className="m_x-20">Localisation</span>
-						<span className="m_x-20">Ville</span>
-						<span className="m_x-20">Pièces</span>
-					</div> */}
-					<button
-						className="btn_p btn color_w br txt_normal m_x-10"
-						onClick={() => router.push("/proprety")}>
-						Trouver une maison
-					</button>
-				</div>
+				<FilterPropretiesSearch />
 			</div>
 		</div>
 	);
