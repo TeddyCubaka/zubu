c3654eefc53c155da5ded8cc89fe80f3a208874b
teddy cubaka <teddycubaka96@gmail.com>
2023-05-19 16:14:31 +0100

    style : config font in the _app file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,12 @@ import "../styles/general_classes.css";
 import "../styles/loader.css";
 import "../styles/mobile.css";
 import "../styles/mobile.globals.css";
-// import "tailwindcss/tailwind.css";
+import "@fontsource/roboto/100.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import "@fontsource/roboto/900.css";
 
 import type { AppProps } from "next/app";
 
