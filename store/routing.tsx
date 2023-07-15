1be5aaf13957c38e5129608762137400d39eb0c7
teddy cubaka <teddycubaka96@gmail.com>
2023-07-15 17:19:13 +0100

    build : passe into maintence

--- store/routing.tsx
+++
@@ -1,7 +0,0 @@
-import { create } from "zustand";
-import { RoutingType } from "../components/interface/routing";
-
-export const routing = create<RoutingType>((set) => ({
-	lastPath: "",
-	_setLastPath: (path) => set(() => ({ lastPath: path })),
-}));
